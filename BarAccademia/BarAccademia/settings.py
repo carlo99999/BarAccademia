83fae53bd06e4181ecf43a1d8404fe6adac9eafd
carlopiccinin_2 <carlo.piccinin@gmail.com>
2024-06-09 22:52:51 +0200

    modified debug

--- BarAccademia/BarAccademia/settings.py
+++ BarAccademia/BarAccademia/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-g)5dcj3l@q!#f^x@v99=@4w#-mn245b*plx&y78717wjrwwqa^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
